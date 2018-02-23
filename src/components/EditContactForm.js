6ed15e648633096b4258a2170ba24787fccac880
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-23 09:17:29 +0100

    Created EditForm and ToggleEditFormButton files, temp content

---
+++ src/components/EditContactForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditContactForm extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                Edit Contact Form
+            </React.Fragment>
+        );
+    }
+}
+
+export default EditContactForm;
---
+++ src/components/ToggleEditContactButton.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import EditContactForm from './EditContactForm';
+
+class ToggleEditContactButton extends Component {
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default ToggleEditContactButton;
