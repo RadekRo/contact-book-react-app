9362533c6fc6d1c88bfc9ce1bb0fa4f82d461df8
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-23 07:04:52 +0100

    Created default state/contact list

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,30 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+
+    state = {
+        contacts: [
+            {
+                name: 'Ann Arrante',
+                phone: '234 324 345',
+                email: 'anna@aol.com',
+                categories: 'work, family'
+            },
+            {
+                name: 'Monica Pascale',
+                phone: '454 324 345',
+                email: 'monicap@gmail.com',
+                categories: 'family'
+            },
+            {
+                name: 'Jessica Hudson',
+                phone: '454 333 345',
+                email: 'jessica333@gmail.com',
+                categories: 'family'
+            }
+        ]
+    };
+
     render() {
         return (
             <React.Fragment>
