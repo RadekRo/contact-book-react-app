c0dcf037d13ed431d9168b282c320a901d55e21d
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-23 09:21:11 +0100

    Toggle visibility of edit form

--- src/components/ToggleEditContactButton.js
+++ src/components/ToggleEditContactButton.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import EditContactForm from './EditContactForm';
 
+state = {
+    showEditForm: true
+};
+
 class ToggleEditContactButton extends Component {
     render() {
         return (
             <React.Fragment>
-
+                <button
+                    onClick={() =>
+                        this.setState({ showEditForm: !this.state.showEditForm })
+                    }
+                >
+                edit
+                </button>
             </React.Fragment>
         );
     }
